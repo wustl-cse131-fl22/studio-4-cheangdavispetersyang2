07867f487f33d5300dfbe545fd3f0aeb71166d19
student <student@DESKTOP-9Q5Q0AV.dhcp.wustl.edu>
2022-10-12 15:40:18 -0500

    The food of choice is curry I guess...

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,23 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.clear();
+		StdDraw.setPenColor(66, 245, 149); //greeeeeen
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.2); //the background
+		
+		
+		StdDraw.setPenColor(245, 84, 66); 
+		StdDraw.setPenColor(0, 0, 0); //0
+		double xLeft[] = {0.5, 0.1, 0.1};
+		double yLeft[] = {0.5, 0.3, 0.7};
+		StdDraw.filledPolygon(xLeft, yLeft);
+		double xRight[] = {0.5, 0.9, 0.9};
+		double yRight[] = {0.5, 0.3, 0.7};
+		StdDraw.filledPolygon(xRight, yRight);
+		StdDraw.setPenColor(245, 84, 66); //orangish red
+		StdDraw.filledCircle(0.5, 0.4, 0.075);
+		StdDraw.filledCircle(0.5, 0.6, 0.075);
+		
 		
 	}
 }
